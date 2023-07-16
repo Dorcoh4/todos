315b06569a6892faeed8fecb4c302bc797f2a7a9
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2023-07-16 18:12:43 +0300

    Create index.js

---
+++ todos-microservice/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Todo = require('./models/todo');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/todos', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+  });
+
+// Create Express app
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.get('/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    console.error('Failed to fetch todos:', error);
+    res.status(500).json({ error: 'Failed to fetch todos' });
+  }
+});
+
+app.post('/todos', async (req, res) => {
+  try {
+    const todo = new Todo(req.body);
+    await todo.save();
+    res.status(201).json(todo);
+  } catch (error) {
+    console.error('Failed to create todo:', error);
+    res.status(500).json({ error: 'Failed to create todo' });
+  }
+});
+
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedTodo = await Todo.findByIdAndUpdate(id, req.body, { new: true });
+    if (!updatedTodo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+    res.json(updatedTodo);
+  } catch (error) {
+    console.error('Failed to update todo:', error);
+    res.status(500).json({ error: 'Failed to update todo' });
+  }
+});
+
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedTodo = await Todo.findByIdAndDelete(id);
+    if (!deletedTodo) {
+      return res.status(404).json({ error: 'Todo not found' });
+    }
+    res.json(deletedTodo);
+  } catch (error) {
+    console.error('Failed to delete todo:', error);
+    res.status(500).json({ error: 'Failed to delete todo' });
+  }
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Todos Microservice running on port ${port}`);
+});
