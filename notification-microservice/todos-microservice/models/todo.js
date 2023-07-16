22dcb30cc91cf3a38ef7550276c37d63f8d1cc6c
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2023-07-16 18:14:41 +0300

    Update todo.js

--- notification-microservice/todos-microservice/models/todo.js
+++ notification-microservice/todos-microservice/models/todo.js
@@ -1,16 +1,1 @@
-const mongoose = require('mongoose');
 
-const todoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  deadline: {
-    type: Date,
-    required: true,
-  },
-});
-
-const Todo = mongoose.model('Todo', todoSchema);
-
-module.exports = Todo;
