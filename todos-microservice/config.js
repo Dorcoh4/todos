a26528670b05f8932147bb4b71eabe073416fab0
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2023-07-16 19:46:18 +0300

    Update config.js

--- todos-microservice/config.js
+++ todos-microservice/config.js
@@ -1,1 +1,5 @@
-batman
+module.exports = {
+  mongoURI: 'mongodb://localhost/todos', // MongoDB connection URI
+  port: 3000, // Port number for the Todos Microservice
+  // Other configuration options...
+};
